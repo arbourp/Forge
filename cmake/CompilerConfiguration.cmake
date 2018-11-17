8e6a281597f78e7a6f46876b8277e5468f70818f
Michael Herwig <michael.herwig@hotmail.de>
2018-11-17 03:15:34 +0100

    Initial Commit

---
+++ cmake/CompilerConfiguration.cmake
@@ -0,0 +1,26 @@
+﻿set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+set(CompilerFlags
+  CMAKE_CXX_FLAGS
+  CMAKE_CXX_FLAGS_DEBUG
+  CMAKE_CXX_FLAGS_RELEASE
+  CMAKE_C_FLAGS
+  CMAKE_C_FLAGS_DEBUG
+  CMAKE_C_FLAGS_RELEASE
+)
+
+if(MSVC)
+    foreach(flag ${CompilerFlags})
+        string(REPLACE "/MDd" "" ${flag} ${${flag}})
+        string(REPLACE "/MD" "" ${flag} ${${flag}})
+        string(REGEX REPLACE "/W[1234X]" "" ${flag} ${${flag}})
+    endforeach()
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W3")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3")
+    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} /MT")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} /MTd")
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd")
+endif()
---
+++ source/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <QApplication>
+
+#include "mainwindow.hpp"
+
+int main(int ac, char **av) {
+  QApplication app{ac, av};
+  MainWindow window{};
+  window.show();
+  app.exec();
+}
